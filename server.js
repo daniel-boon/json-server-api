d2cf751fddada5050f09ba7da3a6839a70d2b208
daniel-boon <61857025+daniel-boon@users.noreply.github.com>
2024-10-23 20:30:01 +0700

    test new db #3

--- server.js
+++ server.js
@@ -4,24 +4,26 @@ const server = jsonServer.create();
 const fs = require('fs');
 const path = require('path');
 const middlewares = jsonServer.defaults();
+
 // Make sure the db.json is only read-only and served from memory to avoid errors when deployed to vercel.
 const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'db/db.json'), 'utf-8'));
 const router = jsonServer.router(data);
 
-
 server.use(middlewares);
 
+// Rewriting custom endpoints
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
+  jsonServer.rewriter({
+    "/api/*": "/$1",        // Keeps the API path if needed
+    "/wildlife": "/animals",  // Renames /animals to /wildlife
+    "/zoo-info": "/zooInfo"   // New endpoint for zoo info
+  })
 );
 
-
 server.use(router);
+
 server.listen(3000, () => {
- console.log("JSON Server is running");
+  console.log("JSON Server is running");
 });
 
 // Export the Server API
